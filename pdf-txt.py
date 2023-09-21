f38b793b6f3222552a4621041b8b26c78fc1e45e
goestfish <114896816+goestfish@users.noreply.github.com>
2023-09-21 13:01:41 -0400

    Update pdf-txt.py
    Signed-off-by: goestfish <114896816+goestfish@users.noreply.github.com>

--- pdf-txt.py
+++ pdf-txt.py
@@ -1,3 +1,4 @@
+import time
 import requests
 import io
 import os
@@ -5,11 +6,19 @@ import re
 from concurrent.futures import ThreadPoolExecutor
 from pdfminer.layout import LAParams
 import pdfplumber
+from retrying import retry
 
-#RE = r'\s*F\s*e\s*d\s*e\s*r\s*a\s*l\s*R\s*e\s*g\s*i\s*s\s*t\s*e\s*r\s*/\s*V\s*o\s*l\s*'
+# RE = r'\s*F\s*e\s*d\s*e\s*r\s*a\s*l\s*R\s*e\s*g\s*i\s*s\s*t\s*e\s*r\s*/\s*V\s*o\s*l\s*'
 RE = r'\s*F\s*[éèêëeE]\s*d\s*[éèêëeE]\s*r\s*a\s*l\s*R\s*[éèêëeE]\s*g\s*[LIil1íìîï]\s*s\s*t\s*[éèêëeE]\s*r\s*/\s*V\s*[Oo0óòôöõøœ]\s*[LIil1íìîï]\s*'
 
 
+@retry(stop_max_attempt_number=3, wait_fixed=2000)
+def fetch_response(url):
+    response = requests.get(url)
+    if response.status_code == 200 and len(response.content) > 1000:
+        return response
+    else:
+        response.raise_for_status()
 
 def fix_vowels_line(line):
     replacements = {
@@ -23,7 +32,6 @@ def fix_vowels_line(line):
     }
     for old, new in replacements.items():
         line = line.replace(old, new)
-
     if re.match(RE, line):
         line = re.sub(r'\s+', '', line)
 
@@ -31,6 +39,7 @@ def fix_vowels_line(line):
 
 
 def get_txt(date, pdf_file, folder_path, word_margin=0.5):
+    print(f"Extracting text from PDF for date: {date}")
     laparams = LAParams(word_margin=word_margin)
     with pdfplumber.open(pdf_file) as pdf:
         lines = []
@@ -46,18 +55,19 @@ def get_txt(date, pdf_file, folder_path, word_margin=0.5):
 
 
 def make_folder(year, month, date, pdf_file):
-    folder_path = f"D:\\pycharm\\pythonProject\\pdf-txt\\FR\\FR-{year}\\{month}"
+    folder_path = f"D:\\pycharm\\pythonProject\\pdf-txt\\FR(miner)\\FR-{year}\\{month}"
+    # folder_path = f"D:\\pycharm\\pythonProject\\pdf-txt\\FR-{year}\\{month}"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     get_txt(date, pdf_file, folder_path)
 
 
 def process_date(year, month, day):
+    global response
     if day < 10:
         day_str = f"0{day}"
     else:
         day_str = str(day)
-
     if month < 10:
         month_str = f"0{month}"
     else:
@@ -65,27 +75,69 @@ def process_date(year, month, day):
 
     date = f"{year}-{month_str}-{day_str}"
     url = f"https://www.govinfo.gov/content/pkg/FR-{date}/pdf/FR-{date}.pdf"
-    response = requests.get(url)
-    if response.status_code == 200 and response.url == url:
+    print(f"Downloading file for date: {date}")
+    try:
+        response = fetch_response(url)
         pdf_file = io.BytesIO(response.content)
         make_folder(year, month_str, date, pdf_file)
         pdf_file.close()
-    response.close()
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to get response for the date {date}, error: {e}")
+    finally:
+        response.close()
+        time.sleep(1)
+
+
+def is_empty(text):
+    return len(text.strip()) == 0
+
+
+def check_and_redownload_empty_files(folder_path):
+    redownloaded_files = []
+    redownload_needed = False
+    for root, ds, fs in os.walk(folder_path):
+        for file_name in fs:
+            if file_name.endswith('.txt'):
+                with open(os.path.join(root, file_name), 'r', encoding='utf-8') as file:
+                    content = file.read(30)
+                    if is_empty(content):
+                        date_info = re.search(r'FR-(\d{4})-(\d{2})-(\d{2})', file_name)
+                        if date_info:
+                            year, month, day = map(int, date_info.groups())
+                            print(f"Redownloading empty file for date: {year}-{month}-{day}")
+                            process_date(year, month, day)
+                            redownloaded_files.append(file_name)
+                            redownload_needed = True
+    if redownload_needed:
+        print("Redownloaded files:")
+        for file in redownloaded_files:
+            print(file)
+    return redownload_needed
 
 
 def main():
-    with ThreadPoolExecutor() as executor:
-        for year in range(1990, 1991):
-            for month in range(1, 2):
-                days_in_month = 31
-                if month == 2:
-                    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-                        days_in_month = 29
-                    else:
-                        days_in_month = 28
-                elif month in [4, 6, 9, 11]:
-                    days_in_month = 30
-                executor.map(process_date, [year] * days_in_month, [month] * days_in_month, range(1, days_in_month + 1))
+    # process_date(1980, 2, 11)
+    try:
+        for year in range(1986, 1992):
+            for month in range(1, 13):
+                with ThreadPoolExecutor(max_workers=3) as executor:
+                    days_in_month = 31
+                    if month == 2:
+                        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+                            days_in_month = 29
+                        else:
+                            days_in_month = 28
+                    elif month in [4, 6, 9, 11]:
+                        days_in_month = 30
+                    executor.map(process_date, [year] * days_in_month, [month] * days_in_month,
+                                 range(1, days_in_month + 1))
+                    for _ in range(3):
+                        redownload_needed = check_and_redownload_empty_files(
+                            f"D:\\pycharm\\pythonProject\\pdf-txt\\FR(miner)\\FR-{year}\\{str(month).zfill(2)}")
+                        if not redownload_needed:
+                            break
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == '__main__':
