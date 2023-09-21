e1d2741c0a5ecfe36d7b7511e4884861a92e39df
goestfish <114896816+goestfish@users.noreply.github.com>
2023-09-22 01:04:44 +0800

    Add files via upload
    Signed-off-by: goestfish <114896816+goestfish@users.noreply.github.com>

---
+++ Find_empty.py
@@ -0,0 +1,16 @@
+import os
+
+folder_path = r'D:\pycharm\pythonProject\pdf-txt\FR(miner)'
+
+
+def is_empty(text):
+    return len(text)==0
+
+with open('Emptyfile2.txt', 'w') as result_file:
+    for root, ds, fs in os.walk(folder_path):
+        for file_name in fs:
+            if file_name.endswith('.txt'):
+                with open(os.path.join(root, file_name), 'r', encoding='utf-8') as file:
+                    content = file.read(30)
+                    if is_empty(content):
+                        result_file.write(file_name + '\n')
